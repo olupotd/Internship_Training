fefc6b2c6109e83af84b85c0a7c2b12a249ccf4e
Olupot Douglas <olupotd@aol.com>
2014-06-14 12:31:13 +0300

    Installed and configured eclipse to read from git

---
+++ src/Image_compressor.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Image_compressor.c
+ Author      : Olupot Douglas
+ Version     :
+ Copyright   : GPL
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
